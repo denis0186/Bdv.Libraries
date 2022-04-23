890536d048347eaface62e293a3febae552b06c8
Denis Bagaev <bdv0186@hotmail.com>
2022-04-24 00:55:36 +0300

    Token dto

---
+++ src/Bdv.Domain/Dto/Sso/TokenDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Bdv.Domain.Dto.Sso
+{
+    /// <summary>
+    /// Token
+    /// </summary>
+    public class TokenDto
+    {
+        /// <summary>
+        /// Token string
+        /// </summary>
+        public string Token { get; set; } = string.Empty;
+    }
+}
